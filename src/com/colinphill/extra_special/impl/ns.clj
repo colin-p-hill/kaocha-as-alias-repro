3d17eb681b4fe37e805ff23043d93d3a54f4b259
Colin P. Hill <66083075+colin-p-hill@users.noreply.github.com>
2023-01-30 11:10:15 -0500

    Small repro

--- src/com/colinphill/extra_special/impl/ns.clj
+++ src/com/colinphill/extra_special/impl/ns.clj
@@ -1,10 +1,7 @@
 (ns com.colinphill.extra-special.impl.ns
   (:refer-clojure :exclude [require symbol])
-  (:require [clojure.core :as c]
-            [clojure.test :as t]
-            [clojure.walk :as walk]
-            [com.colinphill.extra-special.impl.test :as test])
-  (:import (clojure.lang Namespace)))
+  (:require [clojure.test :as t]
+            [com.colinphill.extra-special.impl.test :as test]))
 
 (test/prepare-ns!)
 
