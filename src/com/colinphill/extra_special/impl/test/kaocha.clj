d1205943f2aa4ba15f2ecbc892e6b17f3904e512
Colin P. Hill <66083075+colin-p-hill@users.noreply.github.com>
2023-01-30 11:33:08 -0500

    Small repro

--- src/com/colinphill/extra_special/impl/test/kaocha.clj
+++ src/com/colinphill/extra_special/impl/test/kaocha.clj
@@ -1,4 +1,4 @@
 (ns com.colinphill.extra-special.impl.test.kaocha
-  (:require [com.colinphill.extra-special.impl.ns]))
+  (:require [com.colinphill.extra-special :as-alias es]))
 
 (defn reporter [_] nil)
