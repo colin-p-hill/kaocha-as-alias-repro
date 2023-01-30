a244b595a0d3472c01455ae8b35b93a63e2e2f29
Colin P. Hill <66083075+colin-p-hill@users.noreply.github.com>
2023-01-30 11:06:12 -0500

    Small repro

--- src/com/colinphill/extra_special/impl/test/kaocha.clj
+++ src/com/colinphill/extra_special/impl/test/kaocha.clj
@@ -1,20 +1,7 @@
 (ns com.colinphill.extra-special.impl.test.kaocha
   (:require
    [com.colinphill.extra-special.impl.ns :as ns]
-   [com.colinphill.extra-special.impl.test :as-alias test]
-   [kaocha.report :as report]))
-
-(defmacro ^:private with-print-opts [opts & body]
-  `((requiring-resolve 'lambdaisland.deep-diff2.puget.printer/with-options)
-          ~opts
-          ~@body))
-(defonce ^:private orig (get-method report/print-expr :default))
-(defmethod report/print-expr :default
-  [{:as m :keys [::test/diff]}]
-  (if diff
-    (with-print-opts {:map-coll-separator :line}
-      (report/print-expression m))
-    (orig m)))
+   [com.colinphill.extra-special.impl.test :as-alias test]))
 
 (defn reporter
   "No-op. Only exists to cue Kaocha to load this namespace's `defmethod`s."
