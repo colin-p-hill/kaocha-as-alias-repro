bc75bd5dd3aba1c22f536a9474200c62cd8f1e32
Colin P. Hill <66083075+colin-p-hill@users.noreply.github.com>
2023-01-30 11:07:16 -0500

    Small repro

--- src/com/colinphill/extra_special/impl/test/kaocha.clj
+++ src/com/colinphill/extra_special/impl/test/kaocha.clj
@@ -1,6 +1,6 @@
 (ns com.colinphill.extra-special.impl.test.kaocha
   (:require
-   [com.colinphill.extra-special.impl.ns :as ns]))
+   [com.colinphill.extra-special.impl.ns]))
 
 (defn reporter
   "No-op. Only exists to cue Kaocha to load this namespace's `defmethod`s."
