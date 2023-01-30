761432975be4e18cba9a3f40cf86fa0b6646e7fa
Colin P. Hill <66083075+colin-p-hill@users.noreply.github.com>
2023-01-30 11:39:08 -0500

    CHANGE: Now fails to find any tests until core ns is reloaded.

--- src/com/colinphill/extra_special/impl/ns.clj
+++
@@ -1,6 +0,0 @@
-(ns com.colinphill.extra-special.impl.ns
-  (:require [clojure.test :as t]
-            [com.colinphill.extra-special :as-alias es]))
-
-(t/deftest trivial-preloaded-test
-  (t/is true))
