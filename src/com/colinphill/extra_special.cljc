d2e3ea1fdc73c9b0378604a9604781b9389239b5
Colin P. Hill <66083075+colin-p-hill@users.noreply.github.com>
2023-01-30 11:12:47 -0500

    Small repro

--- src/com/colinphill/extra_special.cljc
+++ src/com/colinphill/extra_special.cljc
@@ -1,6 +1,5 @@
 (ns com.colinphill.extra-special
-  (:require
-   [clojure.test :as t]))
+  (:require [clojure.test :as t]))
 
 (t/deftest trivial-test
   (t/is true))
--- src/com/colinphill/extra_special/impl/test/kaocha.clj
+++ src/com/colinphill/extra_special/impl/test/kaocha.clj
@@ -1,8 +1,4 @@
 (ns com.colinphill.extra-special.impl.test.kaocha
-  (:require
-   [com.colinphill.extra-special.impl.ns]))
+  (:require [com.colinphill.extra-special.impl.ns]))
 
-(defn reporter
-  "No-op. Only exists to cue Kaocha to load this namespace's `defmethod`s."
-  [_]
-  nil)
+(defn reporter [_] nil)
