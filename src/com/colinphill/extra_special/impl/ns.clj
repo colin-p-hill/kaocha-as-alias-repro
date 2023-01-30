58240a3fe8090919b4d3216e53747b948c7413a6
Colin P. Hill <66083075+colin-p-hill@users.noreply.github.com>
2023-01-30 11:12:07 -0500

    Small repro

--- src/com/colinphill/extra_special/impl/ns.clj
+++ src/com/colinphill/extra_special/impl/ns.clj
@@ -1,6 +1,6 @@
 (ns com.colinphill.extra-special.impl.ns
   (:require [clojure.test :as t]
-            [com.colinphill.extra-special.impl.test :as test]))
+            [com.colinphill.extra-special.impl.test]))
 
 (t/deftest trivial-preloaded-test
   (t/is true))
