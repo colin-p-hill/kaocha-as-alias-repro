b863c62ee338203c315702b19c915d024e412ce4
Colin P. Hill <66083075+colin-p-hill@users.noreply.github.com>
2023-01-30 11:23:19 -0500

    Small repro

--- src/com/colinphill/extra_special/impl/test.clj
+++ src/com/colinphill/extra_special/impl/test.clj
@@ -1,8 +1,5 @@
 (ns com.colinphill.extra-special.impl.test
   (:require
-   [clojure.test :as t]
    [com.colinphill.extra-special :as-alias es]))
 
-
-;; Declare aliases unconditionally to satisfy reader
 (require '[expound.alpha :as-alias expound])
